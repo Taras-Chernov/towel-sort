322ff4d5e775a29d637422cf75f8b609f9e94ad6
Taras Chernov <taras.chernov@mail.ru>
2023-08-02 11:21:51 +0300

    Feat: remove ...

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@
     }
     for (let i = 0; i < matrix.length; i++) {
       if (i % 2 === 1) {
-        result = result.concat(...matrix[i].reverse());
+        result = result.concat(matrix[i].reverse());
       } else {
-        result = result.concat(...matrix[i]);
+        result = result.concat(matrix[i]);
       }
     }
     return result
